8e894e326ed1e10f1fe3276d3bd3424b49eacb97
Raja Sahani <srjp2018@gmail.com>
2021-10-27 13:30:22 +0530

    initialcommit

---
+++ QRCodeGenerator/qrcodeapp/models.py
@@ -0,0 +1,22 @@
+# Create your models here.
+from django.db import models
+import qrcode
+from PIL import Image, ImageDraw
+from io import BytesIO
+from django.core.files import File
+import random
+
+class QrCode(models.Model):
+   Qrtxt=models.CharField()
+   image=models.ImageField(upload_to='qrcode',blank=True)
+
+   def save(self,*args,**kwargs):
+      qrcode_img=qrcode.make(self.url)
+      canvas=Image.new("RGB", (300,300),"white")
+      draw=ImageDraw.Draw(canvas)
+      canvas.paste(qrcode_img)
+      buffer=BytesIO()
+      canvas.save(buffer,"PNG")
+      self.image.save(f'image{random.randint(0,9999)}',File(buffer),save=False)
+      canvas.close()
+      super().save(*args,**kwargs)
---
+++ QRCodeGenerator/qrcodeapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import QrCode
+# Create your views here.
+def home(request):
+   if request.method=="POST":
+      Qrtxt=request.POST['qrtext']
+      QrCode.objects.create(url=Qrtxt)
+
+   qr_code=QrCode.objects.all()
+   return render(request,"index.html",{'qr_code':qr_code})
